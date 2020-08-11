2ef0452bc7d40894d054c08709bea97d3f5771fb
Andressa Valengo <valengoandressa@gmail.com>
2020-08-11 17:58:31 -0300

    test

--- app.js
+++ app.js
@@ -16,10 +16,12 @@ if (env === envDevDescription) {
     require('dotenv-safe').config();
 }
 
-console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
 firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+    credential: firebaseAdmin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    }),
     databaseURL: 'https://sigmapa-v2.firebaseio.com'
 });
 
