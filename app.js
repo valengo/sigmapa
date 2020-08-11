0d7d0e88f20798aa6054e05412a133f2fb4b6976
Andressa Valengo <valengoandressa@gmail.com>
2020-08-11 17:33:21 -0300

    Adding firebase

--- app.js
+++ app.js
@@ -10,9 +10,27 @@ const usersRouter = require('./routes/users');
 const mainMapRouter = require('./routes/main-map')
 const loginRouter = require('./routes/login')
 
-const app = express();
+const envDevDescription = 'development'
+let env = process.env.NODE_ENV || envDevDescription
+if (env === envDevDescription) {
+    require('dotenv-safe').config();
+}
 
-console.log(process.env.NODE_ENV);
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+    databaseURL: 'https://sigmapa-v2.firebaseio.com'
+});
+
+function verifyIdToken(idToken) {
+    firebaseAdmin.auth().verifyIdToken(idToken).then(function (decodedToken) {
+        console.log(decodedToken);
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +38,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -31,19 +49,19 @@ app.use('/login', loginRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
