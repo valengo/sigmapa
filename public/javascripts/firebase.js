bed6137fdff891fd6c98cacb0df37aaf4cdeb6ff
Andressa Valengo <valengoandressa@gmail.com>
2020-09-09 12:43:23 -0300

    change to ptbr

--- public/javascripts/firebase.js
+++ public/javascripts/firebase.js
@@ -24,7 +24,6 @@ let uiConfig = {
                     type: 'GET',
                     headers: {Authorization: idToken}
                 }).done(function (){
-                    console.log('done');
                     window.location.replace('/index');
                 });
             }).catch(function(error) {
@@ -35,10 +34,6 @@ let uiConfig = {
     },
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     // tosUrl and privacyPolicyUrl accept either url string or a callback
