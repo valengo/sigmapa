fc8d851651b4dcbde8b8ec6ba80a1e1e7ca94a34
Andressa Valengo <valengoandressa@gmail.com>
2020-09-09 17:29:29 -0300

    logout

--- app.js
+++ app.js
@@ -96,6 +96,11 @@ function configureApp() {
         return res.redirect('/index');
     });
 
+    app.get('/logout', verifyFirebaseIdToken, function (req, res, next) {
+        req.session.destroy();
+        res.redirect('/');
+    });
+
     // catch 404 and forward to error handler
     app.use(function (req, res, next) {
         next(createError(404));
